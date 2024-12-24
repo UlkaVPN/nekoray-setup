69a502e07bcc101865789874d41f0fbbb3f51683
ulka-admin <191695762+ulka-admin@users.noreply.github.com>
2024-12-24 23:06:07 +0300

    Initial commit

---
+++ ps_startup_setup.ps1
@@ -0,0 +1,46 @@
+#### START ELEVATE TO ADMIN #####
+param(
+    [Parameter(Mandatory=$false)]
+    [switch]$shouldAssumeToBeElevated,
+
+    [Parameter(Mandatory=$false)]
+    [String]$workingDirOverride
+)
+
+if (-not ($PSBoundParameters.ContainsKey('workingDirOverride'))) {
+    $workingDirOverride = (Get-Location).Path
+}
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false) {
+    if ($shouldAssumeToBeElevated) {
+        Write-Output "Elevating did not work :("
+
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -shouldAssumeToBeElevated -workingDirOverride "{1}"' -f ($myinvocation.MyCommand.Definition, "$workingDirOverride"))
+    }
+    exit
+}
+
+Set-Location "$workingDirOverride"
+Set-ExecutionPolicy remotesigned
+##### END ELEVATE TO ADMIN #####
+
+
+$currentUser = "$env:USERDOMAIN\$env:USERNAME"
+$programName = "nekobox.exe"
+$taskName = "nekobox Startup Task"
+$taskPath = "\"
+
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$principal = New-ScheduledTaskPrincipal -UserId $currentUser -LogonType Interactive -RunLevel Highest
+$action = New-ScheduledTaskAction -Execute "`"$(Join-Path $scriptDir $programName)`""
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable -ExecutionTimeLimit (New-TimeSpan -Days 7)
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+
+Register-ScheduledTask -TaskName $taskName -TaskPath $taskPath -Principal $principal -Action $action -Settings $settings -Trigger $trigger
